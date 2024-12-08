bdc62e7f9c9c0c087ce69719e91fcdc759d46cc5
David Jadlowkier <davidjadlowkier@Caroline-sin-MacBook-Pro.local>
2024-12-08 03:00:55 +0100

    add content to html, change to separate css docs, add styles

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,21 @@
-// Calling sidebar links
+// CALLING SIDEBAR LINKS
 
 const sidebarLinks = document.querySelectorAll(".sidebar__link");
 
+// CALLING DISPLAY CONTAINER
+const displayContainers = document.querySelectorAll(".content-container");
+console.log(displayContainers);
+
 //loop for switching between tabs from the sidebar
-sidebarLinks.forEach((sidebarLink) => {
+sidebarLinks.forEach((sidebarLink, index) => {
   sidebarLink.addEventListener("click", (e) => {
+    displayContainers.forEach((displayContainer) => {
+      displayContainer.classList.remove("content-container--active");
+    });
     sidebarLinks.forEach((link) => {
       link.classList.remove("sidebar__link--active");
     });
     e.target.classList.add("sidebar__link--active");
+    displayContainers[index].classList.add("content-container--active");
   });
 });
